e90a2f45f5c04bce90c88f186af329467dd205eb
Hardik Mashru <harrymash2006@gmail.com>
2025-06-06 15:39:35 +0530

    Update RNTusClient.m

--- ios/RNTusClient.m
+++ ios/RNTusClient.m
@@ -1,32 +1,16 @@
 #import "RNTusClient.h"
 #import "RNTusClient-Swift.h"
 
-#define ON_SUCCESS @"onSuccess"
-#define ON_ERROR @"onError"
-#define ON_PROGRESS @"onProgress"
-
-@interface RNTusClient ()
-
-@property(nonatomic, strong, readonly) NSMutableDictionary<NSString*, TUSSession*> *sessions;
-@property(nonatomic, strong, readonly) TUSUploadStore *uploadStore;
-@property(nonatomic, strong, readonly) NSMutableDictionary<NSString *, NSString *> *endpoints;
-
-@end
-
 @implementation RNTusClient {
     RNTusClientSwiftBridge *_bridge;
     bool hasListeners;
 }
-
-@synthesize uploadStore = _uploadStore;
 
 RCT_EXPORT_MODULE();
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        _sessions = [NSMutableDictionary new];
-        _endpoints = [NSMutableDictionary new];
         _bridge = [[RNTusClientSwiftBridge alloc] init];
         [_bridge setProgressCallback:^(NSString *uploadId, float progress) {
             if (self->hasListeners) {
@@ -56,39 +40,6 @@ RCT_EXPORT_MODULE();
         }];
     }
     return self;
-}
-
-+ (BOOL)requiresMainQueueSetup {
-  return NO;
-}
-
-- (TUSUploadStore *)uploadStore {
-    if(_uploadStore == nil) {
-        NSFileManager *fileManager = [NSFileManager defaultManager];
-        NSURL *applicationSupportURL = [[fileManager URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask] lastObject];
-        _uploadStore = [[TUSFileUploadStore alloc] initWithURL:[applicationSupportURL URLByAppendingPathComponent:@"__uploadStore.tmp"]];
-    }
-    return _uploadStore;
-}
-
-- (TUSSession *)sessionFor:(NSString *)endpoint {
-    TUSSession *session = [_sessions objectForKey:endpoint];
-    if(session == nil) {
-        session = [[TUSSession alloc] initWithEndpoint:[[NSURL alloc] initWithString:endpoint] dataStore:self.uploadStore allowsCellularAccess:YES];
-        [self.sessions setObject:session forKey:endpoint];
-    }
-    return session;
-}
-
-- (TUSResumableUpload *)restoreUpload:(NSString *)uploadId {
-    NSString *endpoint = self.endpoints[uploadId];
-    TUSSession *session = [self sessionFor:endpoint];
-    return [session restoreUpload:uploadId];
-}
-
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
 }
 
 - (NSArray<NSString *> *)supportedEvents {
@@ -137,14 +88,6 @@ RCT_EXPORT_METHOD(uploadFile:(NSString *)filePath
 RCT_EXPORT_METHOD(cancelUpload:(NSString *)uploadId)
 {
     [_bridge cancelUpload:uploadId];
-}
-
-RCT_EXPORT_METHOD(getUploadProgress:(NSString *)uploadId
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-{
-    double progress = [_bridge getUploadProgress:uploadId];
-    resolve(@(progress));
 }
 
 @end
