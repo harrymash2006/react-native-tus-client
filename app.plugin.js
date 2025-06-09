fd5c8485dca9830c62253b2ae08665ff981621e8
Hardik Mashru <harrymash2006@gmail.com>
2025-06-09 18:11:56 +0530

    updates

--- app.plugin.js
+++ app.plugin.js
@@ -1,3 +1,45 @@
-const withTusUploadPlugin = require('./withTusUploadPlugin');
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = withTusUploadPlugin; 
+const withTusClientAppDelegate = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const filePath = path.join(config.modRequest.platformProjectRoot, 'AppDelegate.mm');
+      const contents = fs.readFileSync(filePath, 'utf-8');
+
+      // Check if the code is already added
+      if (contents.includes('handleEventsForBackgroundURLSession')) {
+        return config;
+      }
+
+      // Add the import
+      const importStatement = '#import "RNTusClient.h"';
+      const newContents = contents.replace(
+        /#import "AppDelegate.h"/,
+        `#import "AppDelegate.h"\n${importStatement}`
+      );
+
+      // Add the method
+      const methodToAdd = `
+- (void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)(void))completionHandler {
+    RNTusClient *tusClient = [self.bridge moduleForClass:[RNTusClient class]];
+    [tusClient registerBackgroundHandler:completionHandler];
+}`;
+
+      // Find the last @end and add our method before it
+      const finalContents = newContents.replace(
+        /@end/,
+        `${methodToAdd}\n\n@end`
+      );
+
+      fs.writeFileSync(filePath, finalContents);
+      return config;
+    },
+  ]);
+};
+
+module.exports = (config) => {
+  return withTusClientAppDelegate(config);
+}; 
